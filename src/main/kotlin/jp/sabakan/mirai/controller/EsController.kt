4ef2412f63298203652d8bb43955835204930a52
iwaisora <Le181ciel@gmail.com>
2025-12-15 11:34:02 +0900

    html作成

--- src/main/kotlin/jp/sabakan/mirai/controller/EsController.kt
+++ src/main/kotlin/jp/sabakan/mirai/controller/EsController.kt
@@ -12,8 +12,18 @@ import org.springframework.web.bind.annotation.PostMapping
             return "entrysheet/es-main"
         }
 
+        @GetMapping("/es-creation")
+        fun getEsCreation(): String{
+            return "entrysheet/es-creation"
+        }
+
         @PostMapping("/es-main")
         fun postIndex(): String{
             return "entrysheet/es-main"
+        }
+
+        @PostMapping("/es-creation")
+        fun postEsCreation(): String{
+            return "entrysheet/es-creation"
         }
     }
