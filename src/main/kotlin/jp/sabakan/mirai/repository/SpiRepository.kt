01925457377e5c936553225fb54898254f565623
s20233085 <20233085-nakayamashinnosuke@hcs.ac.jp>
2025-12-16 11:17:33 +0900

    SPIrepository作成とdata変更

--- src/main/kotlin/jp/sabakan/mirai/repository/SpiRepository.kt
+++ src/main/kotlin/jp/sabakan/mirai/repository/SpiRepository.kt
@@ -1,4 +1,30 @@
 package jp.sabakan.mirai.repository
+import jp.sabakan.mirai.data.SpiData
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.stereotype.Repository
 
+@Repository
 class SpiRepository {
+    @Autowired
+    lateinit var njdbc: NamedParameterJdbcTemplate
+
+    // 指定の問題を取得するSQLクエリ
+    val getQuestion = "SELECT * FROM spi_t WHERE spi_id = :spiId"
+
+    /**
+     * 指定の問題を取得する
+     *
+     * @param data SPIデータ
+     * @return spi問題リスト
+     */
+    fun getUserDetail(data: SpiData): List<Map<String, Any?>> {
+        // パラメータマップの作成
+        val paramMap = mapOf<String, Any?>(
+            "spiId" to data.spiId
+        )
+
+        // クエリの実行
+        return njdbc.queryForList(getQuestion, paramMap)
+    }
 }
