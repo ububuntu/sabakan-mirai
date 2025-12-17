ffa8d07cf02480dda838518f5255b4336af69e41
s20233085 <20233085-nakayamashinnosuke@hcs.ac.jp>
2025-12-17 11:29:48 +0900

    名前リファクタリング

--- src/main/kotlin/jp/sabakan/mirai/controller/ErrorController.kt
+++ src/main/kotlin/jp/sabakan/mirai/controller/ErrorController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
-class errorController {
+class ErrorController {
 
     // 共通のエラー画面
     @GetMapping("/errors")
